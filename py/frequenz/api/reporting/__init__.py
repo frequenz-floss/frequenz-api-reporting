3a03e60457c4278d629b5f572d5188e1e8cf74fc
Stefan Brus <stefan.brus@frequenz.com>
2023-11-24 10:44:43 +0100

    Address TODOs
    Signed-off-by: Stefan Brus <stefan.brus@frequenz.com>

--- py/frequenz/api/reporting/__init__.py
+++ py/frequenz/api/reporting/__init__.py
@@ -2,6 +2,4 @@
 # Copyright © 2023 Frequenz Energy-as-a-Service GmbH
 
 """Frequenz gRPC API to aggregate component data from microgrids.
-
-TODO(cookiecutter): Add a more descriptive module description.
 """
