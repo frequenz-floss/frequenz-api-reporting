15ff486521415e6fe1876e7801b419df2277bb3b
Stefan Brus <stefan.brus@frequenz.com>
2023-11-24 10:37:13 +0100

    Initial commit
    Signed-off-by: Stefan Brus <stefan.brus@frequenz.com>

---
+++ py/frequenz/api/reporting/__init__.py
@@ -0,0 +1,7 @@
+# License: MIT
+# Copyright © 2023 Frequenz Energy-as-a-Service GmbH
+
+"""Frequenz gRPC API to aggregate component data from microgrids.
+
+TODO(cookiecutter): Add a more descriptive module description.
+"""
